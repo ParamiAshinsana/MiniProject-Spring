04cdfb517d9729b4ce0cc2129f379b4b7ff5e633
Parami <paramiashinsana@gmail.com>
2024-02-04 20:40:33 +0530

    checking if Singleton is the default scope of all beans defined in Spring

--- src/main/java/lk/ijse/gdse/aad/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/aad/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.aad;
 
 import lk.ijse.gdse.aad.configs.AppConfig;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -8,6 +9,14 @@ public class AppInitializer {
         var ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
+
+        var myComponent  =ctx.getBean("myComponents");
+        System.out.println(myComponent);
+
+        ConfigurableListableBeanFactory configurableListableBeanFactory =  ctx.getBeanFactory();
+        boolean isSingleton = configurableListableBeanFactory.isSingleton("myComponents");
+        System.out.println(isSingleton);
+
         ctx.close();
     }
 }
