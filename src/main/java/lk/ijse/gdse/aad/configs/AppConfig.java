aa7996e30cea994906ffb460b97c5ad9e254d2fd
Parami <paramiashinsana@gmail.com>
2024-02-04 20:24:29 +0530

    add the @Configuration , @ComponentScan

--- src/main/java/lk/ijse/gdse/aad/configs/AppConfig.java
+++ src/main/java/lk/ijse/gdse/aad/configs/AppConfig.java
@@ -1,5 +1,10 @@
 package lk.ijse.gdse.aad.configs;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.gdse.aad.components")
 public class AppConfig {
 
 }
