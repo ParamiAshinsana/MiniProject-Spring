aab5af947476a1244b16528c6ef9c8e54b30f51b
Parami <paramiashinsana@gmail.com>
2024-02-04 20:27:53 +0530

    create the context

--- src/main/java/lk/ijse/gdse/aad/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/aad/AppInitializer.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse.aad;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer {
     public static void main(String[] args) {
-
+        var ctx = new AnnotationConfigApplicationContext();
     }
 }
