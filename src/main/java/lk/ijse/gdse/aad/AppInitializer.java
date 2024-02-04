1f5722742c34d2d3c8a91c8d442d98def2b938b0
Parami <paramiashinsana@gmail.com>
2024-02-04 20:20:28 +0530

    create the AppConfig Class

--- src/main/java/lk/ijse/gdse/aad/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/aad/AppInitializer.java
@@ -2,6 +2,6 @@ package lk.ijse.gdse.aad;
 
 public class AppInitializer {
     public static void main(String[] args) {
-       
+
     }
 }
---
+++ src/main/java/lk/ijse/gdse/aad/configs/AppConfig.java
@@ -0,0 +1,5 @@
+package lk.ijse.gdse.aad.configs;
+
+public class AppConfig {
+
+}
