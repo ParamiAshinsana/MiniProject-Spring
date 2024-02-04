a1cab229ac723a1c8d29a0ecccc845cfdcc384fd
Parami <paramiashinsana@gmail.com>
2024-02-04 20:29:16 +0530

    registering the AppConfig class inside the ctx

--- src/main/java/lk/ijse/gdse/aad/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/aad/AppInitializer.java
@@ -1,9 +1,11 @@
 package lk.ijse.gdse.aad;
 
+import lk.ijse.gdse.aad.configs.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
         var ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
     }
 }
